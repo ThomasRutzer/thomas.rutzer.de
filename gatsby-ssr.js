376d1a339954d32648ef533ed824cbae54e05cfc
Thomas Rutzer <thomas@rutzer.de>
2019-03-06 22:14:21 +0100

    Removed doubled wrapper from gatsby-ssr config

--- gatsby-ssr.js
+++
@@ -1,5 +0,0 @@
-import React from "react"
-import Layout from "./src/components/layout/layoutWrapper"
-
-export const wrapPageElement = ({ element, props }) => 
-    <Layout {...props}>{element}</Layout>
