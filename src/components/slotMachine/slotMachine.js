04ccdb5818bea9154e53ab83a58d8a1097475aaa
Thomas Rutzer <thomas@rutzer.de>
2019-03-14 23:49:16 +0100

    Update data layer and state of SlotMachine

--- src/components/slotMachine/slotMachine.js
+++ src/components/slotMachine/slotMachine.js
@@ -8,17 +8,49 @@ export default class SlotMachine extends React.Component {
   constructor(props) {
     super(props)
 
+    this.state = {
+      isSpinning: false,
+      jackpot: false
+    }
+
     this.slots = [
-        ['4'],
-        ['0'],
-        ['4']
+        {
+          symbols: ['4'],
+          evaluation: null
+        },
+        {
+          symbols: ['0'],
+          evaluation: null
+        },
+        {
+          symbols: ['4'],
+          evaluation: null
+        }
     ]
+
+    this.resetTimeout = null
 
     this.prepare()
   }
 
+  spin() {
+    this.setState({ isSpinning: true })
+  }
+
+  evaluate(slotIndex, symbol) {
+    this.slots[slotIndex].evaluation = symbol
+
+    return this.slots.filter(slot => slot.evaluation === null).length === 0
+      ? (this.setState({ jackpot: true }), this.resetTimeout(setTimeout(() => this.reset(), 500)))
+      : null
+  }
+
   prepare() {
-    this.slots = this.slots.map(slot => [this.getRandomSymbol(), ...slot, ...this.fillUpSlot(20)]) 
+    this.slots = this.slots.map(slot => 
+        ({ ...slot, symbols: [this.getRandomSymbol(), ...slot.symbols, ...this.fillUpSlot(20)] })
+    ) 
+
+    this.evaluate(0)
   }
 
   fillUpSlot(count) {
@@ -29,13 +61,30 @@ export default class SlotMachine extends React.Component {
     return symbols[Math.floor(Math.random()*symbols.length)]
   }
 
+  reset() {
+    this.setState({
+      jackpot: false,
+      isSpinning: false
+    })
+
+    if (this.resetTimeout) {
+      clearTimeout(this.resetTimeout)
+    }
+  }
+
   render() {
     return (
       <div className={ slotMachineStyles.slotMachine 
       }>
         {
           this.slots.map((slot, index) => 
-            <Slot key={ index } symbols={ this.slots[index] } />
+            <Slot 
+              key={ index } 
+              slotIndex={ index }
+              spin={ this.state.isSpinning }
+              evaluate={ this.evaluate }
+              symbols={ slot.symbols } 
+            />
           )
         }
       </div>
