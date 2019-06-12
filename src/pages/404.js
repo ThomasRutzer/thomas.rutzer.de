055f841e4136a70ab2c3059c7c32392a1ca9ce12
Thomas Rutzer <thomas@rutzer.de>
2019-06-12 23:23:37 +0200

    Add pages/404.js

---
+++ src/pages/404.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { SlotMachine } from '../components/slotMachine/index'
+import LayoutWrapper from '../components/layout/layoutWrapper'
+
+export default () => (
+  <LayoutWrapper>
+    <SlotMachine />
+    <p>Too bad. Unfortunately, I couldn't find where you were looking. But maybe happiness is on your side.</p> 
+  </LayoutWrapper>
+)
