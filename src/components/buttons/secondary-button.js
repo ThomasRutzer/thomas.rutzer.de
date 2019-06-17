9ae3efe7d01ccc3baeb80906651ada28d4eaf5ee
Thomas Rutzer <thomas@rutzer.de>
2019-06-17 22:43:10 +0200

    Update secondary button

---
+++ src/components/buttons/secondary-button.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import buttonStyle from './buttons.module.scss'
+
+export default props => 
+  <button 
+    className={ buttonStyle.secondary }
+    onClick={ props.onClick }
+    disabled={ props.disabled } 
+  >
+    { props.label }
+  </button>
