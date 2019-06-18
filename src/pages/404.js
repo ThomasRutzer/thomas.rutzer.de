8db036ebdc5a84c1b93469dfeb24e1fdddbed4b3
Thomas Rutzer <thomas@rutzer.de>
2019-06-18 23:15:04 +0200

    Update content of 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import { Link } from "gatsby"
 import { SlotMachine } from '../components/slotMachine/index'
 import Copy from "../components/copy/copy"
 import LayoutWrapper from '../components/layout/layoutWrapper'
+import ContentWrapper from '../components/layout/contentWrapper'
 
 const wrapperStyle = {
   display: "flex",
@@ -9,11 +11,26 @@ const wrapperStyle = {
   height: "calc(100vh - 140px)"
 }
 
+const horizontalRuleStyle = {
+  width: "400px",
+  maxWidth: "100%",
+  margin: "0 auto",
+  opacity: "0.2"
+}
+
+const linkStyles = {
+  textDecoration: "underline",
+  color: '#fff'
+}
+
 export default () => (
   <LayoutWrapper>
     <div style={ wrapperStyle }>
+      <ContentWrapper mode={ "small" }>
+        <Copy center={ true }>Too bad <br></br> — I couldn't find what you were looking for. Go <Link to="/" style={ linkStyles }>home</Link> or try your luck.</Copy> 
+        <hr style={ horizontalRuleStyle }></hr>
+      </ContentWrapper>
       <SlotMachine />
-      <Copy>Too bad — I couldn't find what you were looking for. But maybe luck is on your side.</Copy> 
     </div>
   </LayoutWrapper>
 )
