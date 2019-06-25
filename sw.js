8023787a578e4a7bffde4c80b765123e9979e7fd
Thomas Rutzer <thomas@rutzer.de>
2019-06-25 20:35:04 +0200

    Updates

--- sw.js
+++ sw.js
@@ -26,40 +26,40 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-15146ffacb0bdb01f223.js"
+    "url": "webpack-runtime-60cf8b4b92f9ba6e3682.js"
   },
   {
-    "url": "styles.02d3d25877938298700d.css"
+    "url": "styles.4b6d120d1385f405b8fe.css"
   },
   {
-    "url": "styles-2c8f4cbd36dfa00293b5.js"
+    "url": "styles-fde158c3f3dc34bce83c.js"
   },
   {
-    "url": "app-9602ac40286e2e29a6fd.js"
+    "url": "app-44154ba8f19eeded11ac.js"
   },
   {
-    "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-97f7825b824492a4a46f.js"
+    "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-b645255e2082a787bcc9.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "327a3b339aa5e051f823c39d4653ffd5"
+    "revision": "cb31ed2f29e2d96e58ba87ae0b7c0d42"
   },
   {
-    "url": "component---src-pages-404-js-fedd0c4003bea8722f6d.js"
+    "url": "component---src-pages-404-js-a3ea58ffab8503b42cc4.js"
   },
   {
-    "url": "2-6151ec4d4d83a2e45725.js"
+    "url": "2-8ad4150bed010c4144f2.js"
   },
   {
-    "url": "1-5a50e72e6a03c2318b68.js"
+    "url": "1-7e58b9302199e9b67a28.js"
   },
   {
     "url": "page-data/404.html/page-data.json",
-    "revision": "90c225c887cb69b6b3e01a5db74735c1"
+    "revision": "3637048327a79f1fdd372453832b1aeb"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
-    "revision": "f844e9c8f6535ede2fa1629763c015e6"
+    "revision": "fffd90caf33b51b380263d00c2bc3917"
   },
   {
     "url": "manifest.webmanifest",
