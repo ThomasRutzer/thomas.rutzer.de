82f0ba6e87984c4e7d9d81c184a73006b8e5e5e7
Thomas Rutzer <thomas.rutzer@artcom.de>
2020-06-23 23:39:44 +0200

    Style externalLink and create links dir

---
+++ src/components/links/externalLink/externalLink.js
@@ -0,0 +1,36 @@
+import React, { useMemo } from "react"
+import classnames from "classnames"
+
+import "./externalLink.scss"
+
+export default ({ children, link, appearace, size }) => {
+  const createSizeClass = useMemo(() => classnames({ "h2": size === "large" }), [size])
+  const createAppearanceClass = useMemo(
+    () => classnames({ "externalLink--primary text-secondary": appearace === "primary" }), 
+    [appearace]
+  )
+
+  return (
+    <a 
+      className={`externalLink ${createAppearanceClass} ${createSizeClass}`} 
+      href={link} 
+      rel="noopener"
+      target="blank">
+      {children}
+      <svg
+        className="externalLink__icon"
+        xmlns="http://www.w3.org/2000/svg"
+        width="1em"
+        height="1em"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="#000000"
+        strokeWidth="2"
+        strokeLinecap="square"
+        strokeLinejoin="arcs"
+      >
+        <path d="M7 17l9.2-9.2M17 17V7H7" />
+      </svg>
+    </a>
+  )
+}
