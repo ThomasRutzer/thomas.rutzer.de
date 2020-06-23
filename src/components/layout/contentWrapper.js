c120990f61e2328feb7740fe8b36c7426bb76451
Thomas Rutzer <thomas.rutzer@artcom.de>
2020-06-23 16:24:52 +0200

    Remove custom styles from contentWrapper and use tailwind cls instead

--- src/components/layout/contentWrapper.js
+++ src/components/layout/contentWrapper.js
@@ -1,12 +1,31 @@
-import React from 'react'
-import contentWrapperStyles from './contentWrapper.module.scss'
+import React from "react"
+import classnames from "classnames"
 
-export default ({ children, size }) => (
-  <div
-    className={`${contentWrapperStyles.container} ${
-      size ? contentWrapperStyles[size] : null
-      }`}
-  >
-    {children}
-  </div>
-)
+function ContentWrapper({ children, className = "", variant, verticalSpacing = true }) {
+  const createClasses = () => {
+    const widthClass = (variant => {
+      switch(variant) {
+        case 'small':
+          return "max-w-xl";
+        case "large":
+          return "max-w-full xl:max-w-screen-xl";
+        default:
+          return "max-w-4xl";
+      }
+    })(variant);
+
+    return classnames(
+      "relative", "container", "mx-auto", "px-4", "md:px-5",
+      { ["py-7"]: verticalSpacing }, 
+      { [`${ widthClass}`]: true },
+    ) 
+  }
+
+  return (
+    <div className={ `${ createClasses() } ${ className }` }>
+      { children }
+    </div>
+  )
+}
+
+export default ContentWrapper
