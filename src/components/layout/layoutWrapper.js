a69f1f2c6f798075c56e7321196f85d01fda8a7f
Thomas Rutzer <thomas.rutzer@artcom.de>
2020-06-29 21:24:46 +0200

    Update LayoutWrapper

--- src/components/layout/layoutWrapper.js
+++ src/components/layout/layoutWrapper.js
@@ -1,14 +1,10 @@
 import React from 'react'
-import layoutWrapperStyles from './layoutWrapper.module.scss'
 import Footer from './../footer/footer'
 import Header from './../header/header'
 
 export default ({ children, seo }) => (
-  <>
-    <div className={layoutWrapperStyles.layoutWrapper}>
-      <Header />
-      {children}
-      <Footer />
-    </div>
-  </>
+  <div>
+    {children}
+    <Footer />
+  </div>
 )
