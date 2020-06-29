17f361f50b2f57ef1f61c809ed9c0c597288b010
Thomas Rutzer <thomas.rutzer@artcom.de>
2020-06-29 21:23:55 +0200

    Add more content to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,23 +17,35 @@ export default () => (
         <span className="text-secondary">.</span>
       </h3>
       <ImageGrid />
-      <div className="grid grid-cols-6 md:mt-6 md:gap-5">
+      <div className="grid grid-cols-6 md:mt-7 md:gap-5">
         <div className="col-start-1 cols-span-4 md:col-span-1">
-          <FeatureList items={["test1", "test2"]} />
+          <p className="text-grey-darker">2019</p>
+          <FeatureList items={["webapps", "mqtt"]} />
         </div>
         <div className="col-start-1 col-span-6 md:col-start-2 md:col-span-3">
           <p>
-            Lorem ipsum dolet justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+            art+com led the exhibition design of the "futurium", a brand new museum in Berlin.
+            I was part of the team which has been responsible for a solid infrastructure as well as for multiple exhibits itself.
+            The infrastructure includes automated deployment via ansible and a backend with multiple services around a mqtt-broker, which guarantees realtime communication across all the exhibits.
+            Various sensors enhance the user interaction and are also connected to to exhibits through mqtt.
           </p>
         </div>
         <div className="col-start-1 col-span-6 md:col-start-5 md:col-span-2">
           <div className="flex flex-col md:items-end">
             <div className="md:transform md:-rotate-90 md:translate-x-1/2 md:origin-bottom">
               <div>
-                <ExternalLink link={"https://artcom.de/en/project/futurium-permanent-exhibition/"} appearace="primary">art+com case</ExternalLink>
+                <ExternalLink
+                  link="https://artcom.de/en/project/futurium-permanent-exhibition/"
+                  appearace="primary">
+                  art+com case
+                </ExternalLink>
               </div>
               <div>
-                <ExternalLink link={"mailto:thomas@rutzer.de"} appearace="primary">futurium.de</ExternalLink>
+                <ExternalLink
+                  link="https://futurium.de/"
+                  appearace="primary">
+                  futurium.de
+                </ExternalLink>
               </div>
             </div>
           </div>
@@ -41,25 +53,31 @@ export default () => (
       </div>
     </ContentWrapper>
     <ContentWrapper variant="large">
-      <h3 className="text-sm mb-4">
-        <span className="underline">writes</span>
-        <span className="text-secondary">.</span>
-      </h3>
-      <div className="mb-7">
-        <ExternalLink link={"https://blog.diepartments.de/pains-and-gains-when-developing-a-node-js-dialogflow-application/"} size="large">
-          Pains and gains when developing a node.js Dialogflow application
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <div className="col-start-1 col-span-1">
+          <h3 className="text-sm md:mt-4">
+            <span className="underline">writes</span>
+            <span className="text-secondary">.</span>
+          </h3>
+        </div>
+        <div className="col-start-1 md:col-start-2 col-span-2">
+          <div className="mb-7">
+            <ExternalLink link={"https://blog.diepartments.de/pains-and-gains-when-developing-a-node-js-dialogflow-application/"} size="large">
+              Pains and gains when developing a node.js Dialogflow application
         </ExternalLink>
-        <small className="block italic">— Published first time 07.2018 on diepartments' blog. A translated version has been printed in  
+            <small className="block italic">— Published first time 07.2018 on diepartments' blog. A translated version has been printed in
           <ExternalLink link="https://kiosk.entwickler.de/php-magazin/php-magazin-3-2019/"> php magazin 03.2019</ExternalLink>
-        </small>
-      </div>
-      <div>
-        <ExternalLink 
-          link="https://blog.diepartments.de/webvr-webgl-vue-js-2-0-and-aframe-js-a-sweet-couple/" 
-          size="large">
-        vue.js 2.0 and aframe.js for WebVR & 3D WebGL applications
+            </small>
+          </div>
+          <div>
+            <ExternalLink
+              link="https://blog.diepartments.de/webvr-webgl-vue-js-2-0-and-aframe-js-a-sweet-couple/"
+              size="large">
+              vue.js 2.0 and aframe.js for WebVR & 3D WebGL applications
         </ExternalLink>
-        <small className="block italic">— Published 02.2017</small>
+            <small className="block italic">— Published 02.2017</small>
+          </div>
+        </div>
       </div>
     </ContentWrapper>
     <section className="bg-secondary">
@@ -124,13 +142,25 @@ export default () => (
             <h5 className="mb-2">images</h5>
             <ul>
               <li>
-                <ExternalLink link={""} size="small">Futurium <i>permanent exhibition</i> 01</ExternalLink>
+                <ExternalLink
+                  link="https://futurium.de/uploads/images/Futurium_David_von_Becker_VB_4593.jpg"
+                  size="small">
+                  Fig.1: Futurium <i>permanent exhibition</i> by David von Becker
+                  </ExternalLink>
               </li>
               <li>
-                <ExternalLink link={""} size="small">Futurium <i>permanent exhibition</i> 02</ExternalLink>
+                <ExternalLink 
+                  link="https://futurium.de/uploads/images/FUT_Eroeffnung_Jan_Windszus_06092019_01734.jpg"
+                  size="small">
+                  Fig.2: Futurium <i>permanent exhibition</i> by Jan Windszus
+                </ExternalLink>
               </li>
               <li>
-                <ExternalLink link={""} size="small">Futurium <i>permanent exhibition</i> 03</ExternalLink>
+                <ExternalLink 
+                  link="https://futurium.de/uploads/images/Futurium_David_von_Becker_VB_9824.jpg"
+                  size="small">
+                    Fig.3: Futurium <i>permanent exhibition</i> by David von Becker
+                </ExternalLink>
               </li>
             </ul>
           </div>
