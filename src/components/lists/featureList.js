dd2638b8c6e5650a2d0fd6b55e53463b704a1a78
Thomas Rutzer <thomas.rutzer@artcom.de>
2020-06-29 21:24:59 +0200

    Update styling of FeatureList

--- src/components/lists/featureList.js
+++ src/components/lists/featureList.js
@@ -5,7 +5,7 @@ import "./featureList.scss"
 const FeatureList = ({ items }) => {
   return (
     <ul className="featureList pl-2">
-      {items.map(item => <li className="featureList__item text-highlight">{item}</li>)}
+      {items.map(item => <li className="featureList__item text-secondary">{item}</li>)}
     </ul>
   )
 }
