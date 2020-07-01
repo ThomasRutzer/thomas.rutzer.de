9f6764461a8da348ebbc9d34f0ee61587c8479bd
Thomas Rutzer <thomas.rutzer@artcom.de>
2020-07-01 18:16:22 +0200

    Add prop key to FeatureList

--- src/components/lists/featureList.js
+++ src/components/lists/featureList.js
@@ -5,7 +5,9 @@ import "./featureList.scss"
 const FeatureList = ({ items }) => {
   return (
     <ul className="featureList pl-2">
-      {items.map(item => <li className="featureList__item text-secondary">{item}</li>)}
+      {items.map((item, key) => 
+        <li key={key} className="featureList__item text-secondary">{item}</li>
+      )}
     </ul>
   )
 }
