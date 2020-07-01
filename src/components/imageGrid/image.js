d77f4c3f4289568d1d8d6f03f3c6ccafb2e008b3
Thomas Rutzer <thomas.rutzer@artcom.de>
2020-07-01 18:16:06 +0200

    Add gatsby-image to ImageGrid

--- src/components/imageGrid/image.js
+++ src/components/imageGrid/image.js
@@ -1,7 +1,8 @@
-import React, { useMemo } from "react"
+import React, { useMemo, useRef, useEffect, useState } from "react"
 import classnames from "classnames"
+import Img from "gatsby-image"
 
-const Image = ({ src, mobileRow, tiles, index }) => {
+const Image = ({ src, alt, fit = "cover", mobileRow, tiles, index, offsetX = 0, offsetY = 0 }) => {
   const createTilesClasses = useMemo(() => classnames({
     [`col-start-${index + 1} col-span-1`]: true,
     [`row-start-${mobileRow} row-span-1`]: true,
@@ -10,11 +11,27 @@ const Image = ({ src, mobileRow, tiles, index }) => {
     [`md:row-start-${tiles[1][0]}`]: true,
     [`md:row-span-${tiles[1][1]}`]: true
   }), [tiles[0][0], tiles[0][1], tiles[1][0], tiles[1][1]])
-  
+
   return (
-    <div className={`${createTilesClasses} overflow-hidden flex items-center`}>
-      <img className="w-full h-full object-cover"
-        src="https://images.unsplash.com/photo-1567653418876-5bb0e566e1c2?ixlib=rb-1.2.1&auto=format&fit=crop&w=1300&q=80" />
+    <div
+      style={{
+        transform: `translate(${offsetX}px, ${offsetY}px)`
+      }}
+      className={
+        `${createTilesClasses} 
+        overflow-hidden bg-grey-darker
+        flex items-center relative transition-transform duration-1000 ease-out`
+      }>
+      <Img
+        className="w-full h-full"
+        imgStyle={{
+          "objectFit": fit
+        } }
+        fluid={src.childImageSharp.fluid}
+        alt={alt} />
+      <figcaption className="absolute bg-grey bottom-0 right-0 px-1 text-xs text-grey-darker">
+        Fig.{index + 1}
+      </figcaption>
     </div>
   )
 }
--- src/components/imageGrid/index.js
+++ src/components/imageGrid/index.js
@@ -1,18 +1,46 @@
-import React from "react"
+import React, { useEffect, useState, useRef, useCallback } from "react"
 
+import getMousePosFromEvent from "./../../utils/getMousePosFromEvent"
+import getRandomNumber from "./../../utils/getRandomNumber"
+import lerp from "./../../utils/lerp"
+import map from "./../../utils/map"
 import Image from "./image"
 
-const ImageGrid = ({ }) => {
+const ImageGrid = ({ title, subtitle, images = [] }) => {
+  const [tx, setTx] = useState(0)
+  const [ty, setTy] = useState(0)
+
+  const mousemoveHandler = useCallback(e => {
+    const xstart = getRandomNumber(5, 30)
+    const ystart = getRandomNumber(5, 15)
+    const { x: mouseX, y: mouseY } = getMousePosFromEvent(e)
+    setTx(prevTx => lerp(prevTx, map(mouseX, 0, window.innerWidth, -xstart, xstart), 0.07))
+    setTy(prevTy => lerp(prevTy, map(mouseY, 0, window.innerHeight, -ystart, ystart), 0.07))
+  }, [])
+
+  useEffect(() => {
+    document.addEventListener('mousemove', mousemoveHandler)
+
+    return () => document.removeEventListener('mousemove', mousemoveHandler)
+  }, [])
+
   return (
     <div className="relative">
-      <div className="mb-6 md:absolute md:w-full md:h-full flex items-center justify-center flex-col">
-        <h2 className="text-6xl">Futurium</h2>
-        <h3>Permanent Exhibition</h3>
+      <div className="mb-6 md:absolute md:w-full md:h-full flex items-center justify-center flex-col z-10">
+        <h2 className="text-6xl border-solid border-b-4">{title}</h2>
+        <h3 className="mt-4">{subtitle}</h3>
       </div>
       <div className="grid grid-cols-3 grid-rows-2 md:grid-cols-6 md:grid-rows-3 md:gap-5 md:h-screen">
-        <Image index={0} mobileRow="2" tiles={[[1, 2], [2, 4]]} />
-        <Image index={1} mobileRow="1" tiles={[[4, 2], [1, 1]]} />
-        <Image index={2} mobileRow="1" tiles={[[6, 1], [2, 1]]} />
+        {images.map((image, key) =>
+          <Image
+            index={key}
+            src={image.src}
+            alt={image.alt}
+            fit={image.fit}
+            tiles={image.tiles}
+            mobileRow={image.mobileRow}
+            offsetX={tx} offsetY={ty} />
+        )}
       </div>
     </div>
   )
