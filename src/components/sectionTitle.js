4fade799a8e54470a2955c8d1e5707d081e5eebe
Thomas Rutzer <thomas.rutzer@artcom.de>
2020-07-18 17:06:57 +0200

    Update sectionTitle

--- src/components/sectionTitle.js
+++ src/components/sectionTitle.js
@@ -1,25 +1,14 @@
 import React from "react"
-import classnames from "classnames"
 
 const SectionTitle = ({ children, appearance }) => {
-  const createClasses = () => {
-    return classnames(
-      "italic", "text-stroke-1", "text-transparent",
-      { ["text-stroke-white"]: appearance !=="primary" },
-      { ["text-stroke-tertiary"]: appearance ==="primary" },
-    )
-  }
-
   return (
-    <h3 className="text-3xl">
-      <span className={createClasses()}>
+    <h3 className="text-2xl text-grey-lighter">
+      <span>
         {children}
       </span>
       <span className="text-primary">.</span>
     </h3>
   )
 }
-
-
 
 export default SectionTitle
