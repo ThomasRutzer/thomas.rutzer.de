d379bd87480992805c8d6777d5d660e01e20981a
Thomas Rutzer <thomas.rutzer@artcom.de>
2020-07-27 12:51:07 +0200

    Add component parallaxCircles

---
+++ src/components/parallax-circles/index.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+import { useMousemoveTranslation } from "./../../hooks"
+
+const ParallaxCircles = () => {
+  const translations = useMousemoveTranslation(10, 20, 10, 20)
+
+  return (
+    <div className="parallax-circles" aria-hidden>
+      <div className="parallax-circles__outer">
+        <div
+          style={{
+            transform: `translate(${translations.tx}px, ${translations.ty}px)`
+          }}
+          className="parallax-circles__inner  transition-transform duration-700 ease-out"></div>
+      </div>
+    </div>
+  )
+}
+
+export default ParallaxCircles
