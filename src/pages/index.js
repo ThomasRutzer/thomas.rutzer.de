71536abdecba613da62c36aee94fd1b8ee9f4097
Thomas Rutzer <thomas.rutzer@artcom.de>
2020-07-29 15:43:43 +0200

    Format index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,172 +8,172 @@ import { ExternalLink } from "../components/links"
 import { SectionDigArchive, SectionTitle, WorkSectionWithImageGrid } from "./../components/sections"
 
 export default ({ data }) => {
-    return (
-        <LayoutWrapper>
-            <Seo />
-            <Intro imgPath={data.portrait.childImageSharp.fluid} />
-            <ContentWrapper
-                verticalSpacing={false}
-                additionalClasses="pt-4 md:pt-7">
-                <SectionTitle>works</SectionTitle>
-            </ContentWrapper>
-            {data.allWorksJson.edges.map((work, key) =>
-                <WorkSectionWithImageGrid
-                    work={work.node}
-                    key={key} />
-            )}
-            <SectionDigArchive />
-            <section className="bg-grey-darkest-pattern">
-                <ContentWrapper>
-                    <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                        <div className="col-start-1 col-span-1">
-                            <SectionTitle appearance="primary">thoughts</SectionTitle>
-                        </div>
-                        <div className="col-start-1 md:col-start-2 col-span-2">
-                            {data.allWritesJson.edges.map((write, key) =>
-                                <div key={key} className={key === 0 ? "mb-4 md:mb-7" : "mb-0"}>
-                                    <ExternalLink link={write.node.link} appearance="primary" size="large">
-                                        {write.node.title}
-                                    </ExternalLink>
-                                    <small className="block italic text-grey-lighter mt-4">— {write.node.description}
-                                    </small>
-                                </div>
-                            )}
-                        </div>
-                    </div>
-                </ContentWrapper>
-            </section>
-            <section className="bg-tertiary">
-                <ContentWrapper>
-                    <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                        <div className="col-start-1 col-span-1">
-                            <SectionTitle>about</SectionTitle>
-                        </div>
-                        <div className="col-start-1 md:col-start-2 col-span-1">
-                            <p>
-                                I'm a leading JavaScript developer with a focus on unique interfaces and interactions, currently working at <ExternalLink link={"https://artcom.de"} appearance="primary">art+com</ExternalLink>, a studio for media installations and spaces.
+  return (
+    <LayoutWrapper>
+      <Seo />
+      <Intro imgPath={data.portrait.childImageSharp.fluid} />
+      <ContentWrapper
+        verticalSpacing={false}
+        additionalClasses="pt-4 md:pt-7">
+        <SectionTitle>works</SectionTitle>
+      </ContentWrapper>
+      {data.allWorksJson.edges.map((work, key) =>
+        <WorkSectionWithImageGrid
+          work={work.node}
+          key={key} />
+      )}
+      <SectionDigArchive />
+      <section className="bg-grey-darkest-pattern">
+        <ContentWrapper>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            <div className="col-start-1 col-span-1">
+              <SectionTitle appearance="primary">thoughts</SectionTitle>
+            </div>
+            <div className="col-start-1 md:col-start-2 col-span-2">
+              {data.allWritesJson.edges.map((write, key) =>
+                <div key={key} className={key === 0 ? "mb-4 md:mb-7" : "mb-0"}>
+                  <ExternalLink link={write.node.link} appearance="primary" size="large">
+                    {write.node.title}
+                  </ExternalLink>
+                  <small className="block italic text-grey-lighter mt-4">— {write.node.description}
+                  </small>
+                </div>
+              )}
+            </div>
+          </div>
+        </ContentWrapper>
+      </section>
+      <section className="bg-tertiary">
+        <ContentWrapper>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            <div className="col-start-1 col-span-1">
+              <SectionTitle>about</SectionTitle>
+            </div>
+            <div className="col-start-1 md:col-start-2 col-span-1">
+              <p>
+                I'm a leading JavaScript developer with a focus on unique interfaces and interactions, currently working at <ExternalLink link={"https://artcom.de"} appearance="primary">art+com</ExternalLink>, a studio for media installations and spaces.
               </p>
-                            <p className="mt-4">
-                                In my team located in 50°56"52.8"N 6°54"48.7"E, we are specialized in crafting custom solutions, mostly with full-stack JavaScript and MQTT based realtime communication.
+              <p className="mt-4">
+                In my team located in 50°56"52.8"N 6°54"48.7"E, we are specialized in crafting custom solutions, mostly with full-stack JavaScript and MQTT based realtime communication.
               </p>
-                            <p className="mt-4">
-                                My personal research focus at the moment is blending interfaces into spaces with the new web XR API.
+              <p className="mt-4">
+                My personal research focus at the moment is blending interfaces into spaces with the new web XR API.
               </p>
-                        </div>
-                        <div className="col-start-1 md:col-start-3 col-span-1">
-                            <div className="flex flex-col md:items-end">
-                                <div className="md:transform md:-rotate-90 md:-translate-y-full md:origin-bottom-right md:pl-2">
-                                    <div>
-                                        <ExternalLink
-                                            additionalClasses="whitespace-no-wrap"
-                                            appearance="primary"
-                                            link={"twitter.com/thomasrutzer"}
-                                            size="large">
-                                            twitter
+            </div>
+            <div className="col-start-1 md:col-start-3 col-span-1">
+              <div className="flex flex-col md:items-end">
+                <div className="md:transform md:-rotate-90 md:-translate-y-full md:origin-bottom-right md:pl-2">
+                  <div>
+                    <ExternalLink
+                      additionalClasses="whitespace-no-wrap"
+                      appearance="primary"
+                      link={"twitter.com/thomasrutzer"}
+                      size="large">
+                      twitter
                 </ExternalLink>
-                                    </div>
-                                    <div>
-                                        <ExternalLink
-                                            additionalClasses="whitespace-no-wrap"
-                                            appearance="primary"
-                                            link={"mailto:thomas@rutzer.de"}
-                                            size="large">
-                                            mail
+                  </div>
+                  <div>
+                    <ExternalLink
+                      additionalClasses="whitespace-no-wrap"
+                      appearance="primary"
+                      link={"mailto:thomas@rutzer.de"}
+                      size="large">
+                      mail
                 </ExternalLink>
-                                    </div>
-                                    <div>
-                                        <ExternalLink
-                                            additionalClasses="whitespace-no-wrap"
-                                            appearance="primary"
-                                            link={"https://github.com/ThomasRutzer"}
-                                            size="large">
-                                            github
+                  </div>
+                  <div>
+                    <ExternalLink
+                      additionalClasses="whitespace-no-wrap"
+                      appearance="primary"
+                      link={"https://github.com/ThomasRutzer"}
+                      size="large">
+                      github
                 </ExternalLink></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </ContentWrapper>
-            </section>
-            <section className="bg-grey-darkest-pattern">
-                <ContentWrapper>
-                    <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                        <div className="col-start-1 col-span-1">
-                            <SectionTitle appearance="primary">credits</SectionTitle>
-                        </div>
-                        <div className="col-start-1 md:col-start-2 col-span-1">
-                            <p className="mb-2 font-bold">images</p>
-                            {data.allWorksJson.edges.map((work, key) =>
-                                <div key={key} className={key === 0 ? "mt-0" : "mt-4"}>
-                                    <p className="text-sm text-tertiary-lighter"
-                                    >{work.node.title} <i>{work.node.subTitle}</i>
-                                    </p>
-                                    <ul>
-                                        {work.node.images.map((image, imageKey) =>
-                                            <li key={imageKey}>
-                                                {image.reference.link &&
-                                                    <ExternalLink
-                                                        link={image.reference.link}
-                                                        size="small">
-                                                        Fig.{imageKey + 1} by {image.reference.author}
-                                                    </ExternalLink>
-                                                }
-                                                {!image.reference.link &&
-                                                    <p className="text-sm">
-                                                        Fig.{imageKey + 1} by {image.reference.author}
-                                                    </p>
-                                                }
-                                            </li>
-                                        )}
-                                    </ul>
-                                </div>
-                            )}
-                        </div>
-                        <div className="col-start-1 md:col-start-3 col-span-1">
-                            <p className="mb-2 font-bold">this site</p>
-                            <ul>
-                                <li>
-                                    <ExternalLink
-                                        link={"https://www.gatsbyjs.org/"}
-                                        size="small">
-                                        Gatsby.js
+                </div>
+              </div>
+            </div>
+          </div>
+        </ContentWrapper>
+      </section>
+      <section className="bg-grey-darkest-pattern">
+        <ContentWrapper>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            <div className="col-start-1 col-span-1">
+              <SectionTitle appearance="primary">credits</SectionTitle>
+            </div>
+            <div className="col-start-1 md:col-start-2 col-span-1">
+              <p className="mb-2 font-bold">images</p>
+              {data.allWorksJson.edges.map((work, key) =>
+                <div key={key} className={key === 0 ? "mt-0" : "mt-4"}>
+                  <p className="text-sm text-tertiary-lighter"
+                  >{work.node.title} <i>{work.node.subTitle}</i>
+                  </p>
+                  <ul>
+                    {work.node.images.map((image, imageKey) =>
+                      <li key={imageKey}>
+                        {image.reference.link &&
+                          <ExternalLink
+                            link={image.reference.link}
+                            size="small">
+                            Fig.{imageKey + 1} by {image.reference.author}
+                          </ExternalLink>
+                        }
+                        {!image.reference.link &&
+                          <p className="text-sm">
+                            Fig.{imageKey + 1} by {image.reference.author}
+                          </p>
+                        }
+                      </li>
+                    )}
+                  </ul>
+                </div>
+              )}
+            </div>
+            <div className="col-start-1 md:col-start-3 col-span-1">
+              <p className="mb-2 font-bold">this site</p>
+              <ul>
+                <li>
+                  <ExternalLink
+                    link={"https://www.gatsbyjs.org/"}
+                    size="small">
+                    Gatsby.js
               </ExternalLink>
-                                </li>
-                                <li>
-                                    <ExternalLink
-                                        link={"https://tailwindcss.com/"}
-                                        size="small">
-                                        Tailwind.css
+                </li>
+                <li>
+                  <ExternalLink
+                    link={"https://tailwindcss.com/"}
+                    size="small">
+                    Tailwind.css
                   </ExternalLink>
-                                </li>
-                                <li>
-                                    <ExternalLink
-                                        link={"https://animejs.com/"}
-                                        size="small">
-                                        anime.js
+                </li>
+                <li>
+                  <ExternalLink
+                    link={"https://animejs.com/"}
+                    size="small">
+                    anime.js
                   </ExternalLink>
-                                </li>
-                                <li>
-                                    <ExternalLink
-                                        link={"https://github.com/features/actions"}
-                                        size="small">
-                                        Github actions
+                </li>
+                <li>
+                  <ExternalLink
+                    link={"https://github.com/features/actions"}
+                    size="small">
+                    Github actions
               </ExternalLink>
-                                </li>
-                                <li>
-                                    <ExternalLink
-                                        link={"https://fonts.google.com/specimen/IBM+Plex+Mono"}
-                                        size="small">
-                                        IBM Plex Mono
+                </li>
+                <li>
+                  <ExternalLink
+                    link={"https://fonts.google.com/specimen/IBM+Plex+Mono"}
+                    size="small">
+                    IBM Plex Mono
               </ExternalLink>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </ContentWrapper>
-            </section>
-        </LayoutWrapper>
-    )
+                </li>
+              </ul>
+            </div>
+          </div>
+        </ContentWrapper>
+      </section>
+    </LayoutWrapper>
+  )
 }
 
 export const query = graphql`
