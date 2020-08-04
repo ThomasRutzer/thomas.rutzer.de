43efa8b95974be2d8f529c5224d2539d890436f8
Thomas Rutzer <thomas.rutzer@artcom.de>
2020-08-04 17:40:49 +0200

    Increase spacing on mobile fo SectionDigArchive

--- src/components/sections/sectionDigArchive.js
+++ src/components/sections/sectionDigArchive.js
@@ -7,7 +7,7 @@ import ParallaxCircles from "./../parallaxCircles"
 const SectionDigArchive = () =>
   <section className="section-dig-archive md:py-4">
     <ContentWrapper>
-      <div className="section-dig-archive__content grid grid-cols-1 md:grid-cols-3 md:py-4">
+      <div className="section-dig-archive__content grid grid-cols-1 md:grid-cols-3 pb-4 md:py-4">
         <div className="col-start-1 md:col-start-2 col-span-2 relative">
           <div className="section-dig-archive__circle">
             <ParallaxCircles />
