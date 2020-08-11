421ae66083d1bc400ff3190aa5a19bd6a068dc8a
Thomas Rutzer <thomas.rutzer@artcom.de>
2020-08-11 18:52:23 +0200

    Update template of accordion

--- src/components/accordion/accordionItem.js
+++ src/components/accordion/accordionItem.js
@@ -12,16 +12,16 @@ const AccordionItem = ({ children, title, subTitle, imgSrc, imgAlt }) => {
     <AccessibleAccordionItem
       activeClassName="accordion-item accordion-item--expanded"
       className="accordion-item">
-      <div className="accordion-item__content-wrapper flex flex-col max-w-screen-md">
+      <div className="accordion-item__content-wrapper flex flex-col">
         <AccordionItemHeading className="accordion-item__heading">
           <AccordionItemButton className=" accordion-item__title flex flex-row items-center">
             <AccordionImage
               alt={imgAlt}
               src={imgSrc}
               className="accordion-item__image" />
-            <div className="ml-4">
-              <h2 className="text-3xl md:5xl underline underline-under">{title}</h2>
-              <h3 className="text-xl">{subTitle}</h3>
+            <div className="ml-4 -mt-3">
+              <h2 className="text-3xl md:text-5xl md:text-stroke md:text-stroke-white">{title}</h2>
+              <h3>{subTitle}</h3>
             </div>
           </AccordionItemButton>
         </AccordionItemHeading>
