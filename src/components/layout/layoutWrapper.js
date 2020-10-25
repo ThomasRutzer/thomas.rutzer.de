e4d758451b7ef9b90f758cef7982c91f0987a5e3
Thomas Rutzer <thomas.rutzer@artcom.de>
2020-10-25 12:39:17 +0100

    Remove unnecessary div in layoutWrapper

--- src/components/layout/layoutWrapper.js
+++ src/components/layout/layoutWrapper.js
@@ -4,10 +4,10 @@ import "typeface-ibm-plex-mono"
 import Footer from "./../footer"
 
 const LayoutWrapper = ({ children }) => (
-  <div>
+  <>
     {children}
     <Footer />
-  </div>
+  </>
 )
 
 export default LayoutWrapper
