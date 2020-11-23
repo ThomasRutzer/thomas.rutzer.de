b8ee213006fd34ab4d4850c02336535fea46e4bb
Thomas Rutzer <thomas.rutzer@artcom.de>
2020-11-23 16:03:17 +0100

    Update metadata in gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ const path = require(`path`)
 module.exports = {
   pathPrefix: "/",
   siteMetadata: {
-    title: "thomas — creative developer",
+    title: "thomas rutzer — creative developer and foo/bar",
     description: "I’m thomas rutzer, leading JavaScript developer with focus on unique interfaces and interactions"
   },
   plugins: [
@@ -44,8 +44,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: "thomas — creative developer and foo/bar",
-        short_name: "thomas — creative developer and foo/bar",
+        name: "thomas rutzer — creative developer and foo/bar",
+        short_name: "thomas rutzer — creative developer and foo/bar",
         start_url: "/",
         background_color: "#2e2d2d",
         theme_color: "#2e2d2d",
