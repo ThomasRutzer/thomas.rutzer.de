572936ff786a627dbc290514f43de39be403024b
Thomas Rutzer <thomas.rutzer@artcom.de>
2020-11-24 23:37:34 +0100

    Remove Accordeon from project archive tabs

--- src/pages/project-archive.js
+++ src/pages/project-archive.js
@@ -3,30 +3,61 @@ import { graphql } from "gatsby"
 
 import Seo from "../components/seo"
 import { ContentWrapper, LayoutWrapper } from "../components/layout"
-import PageHeader from "../components/pageHeader"
-import { Accordion, AccordionItem } from "../components/accordion"
 import { ExternalLink } from "../components/links"
 import FeatureList from "../components/featureList"
 import Tabs from "../components/tabs"
 import SectionTitle from "../components/sections/sectionTitle"
 import ProjectArchiveIntro from "../components/sections/projectArchiveIntro"
+import Img from "gatsby-image"
 
 export default ({ data }) => {
   return (
     <>
       <Seo title="project archive" />
       <LayoutWrapper>
-       <ProjectArchiveIntro />
+        <ProjectArchiveIntro />
         <ContentWrapper>
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            <div className="col-start-1 col-span-1">
-              <SectionTitle appearance="primary">by year</SectionTitle>
-            </div>
-            <div className="col-start-1 md:col-start-2 col-span-2">
-              <Tabs defaultIndex={Math.floor(Math.random() * data.allWorksJson.group.length - 1) + 1}>
-                {data.allWorksJson.group.map((group, key) =>
-                  <section key={key} label={`${group.edges[0].node.year}`}>
-                    <Accordion>
+          <Tabs defaultIndex={Math.floor(Math.random() * data.allWorksJson.group.length - 1) + 1}>
+            {data.allWorksJson.group.map((group, key) =>
+              <section key={key} label={`${group.edges[0].node.year}`}>
+                {group.edges.map((work, groupKey) =>
+                  <div
+                    key={groupKey}
+                    className={
+                      `grid grid-cols-4 gap-3 md:gap-4 pb-5 ${groupKey !== 0 ? "pt-5 border-t-2 border-grey-lighter" : ""}`
+                    }>
+                    <div className="col-start-1 col-span-2 md:col-span-1">
+                      <Img
+                        alt={work.node.teaserImage.alt}
+                        fluid={work.node.fields.teaserImageAsset.childImageSharp.fluid} />
+                    </div>
+                    <div className="col-start-3 col-span-2 md:col-span-1 flex flex-col md:justify-between">
+                      <h2 className="text-3xl md:text-4xl">{work.node.title}</h2>
+                      <h3 className="text-tertiary-lighter">{work.node.subTitle}</h3>
+                    </div>
+                    <div className="col-start-1 col-span-4 md:col-span-1 my-3">
+                      <p>{work.node.teaserDescription}</p>
+                    </div>
+                    <div className="col-start-1 col-span-2 md:col-span-1">
+                      <span className="text-tertiary-lighter block text-sm">
+                        key facts
+                          </span>
+                      <FeatureList items={work.node.features} />
+                    </div>
+                    <div className="col-start-3 col-span-2">
+                      {work.node.links.map(({ link, label }, key) =>
+                        <div className="whitespace-no-wrap text-right" key={key}>
+                          <ExternalLink
+                            appearance="primary"
+                            link={link}>
+                            {label}
+                          </ExternalLink>
+                        </div>
+                      )}
+                    </div>
+                  </div>
+                )}
+                {/* <Accordion>
                       {group.edges.map((work, key) =>
                         <AccordionItem
                           key={key}
@@ -58,12 +89,10 @@ export default ({ data }) => {
                           </div>
                         </AccordionItem>
                       )}
-                    </Accordion>
-                  </section>
-                )}
-              </Tabs>
-            </div>
-          </div>
+                    </Accordion> */}
+              </section>
+            )}
+          </Tabs>
         </ContentWrapper>
         <section className="bg-grey-darkest-pattern">
           <ContentWrapper>
@@ -135,8 +164,8 @@ query {
           fields {
             teaserImageAsset {
               childImageSharp {
-                fixed(width: 120, height: 160, quality: 90) {
-                  ...GatsbyImageSharpFixed_withWebp
+                fluid(maxWidth: 400, quality: 100) {
+                  ...GatsbyImageSharpFluid_withWebp
                 }
               }
             }
