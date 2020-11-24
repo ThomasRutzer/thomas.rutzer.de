067831c34e9e5c31aba9d56b3b10b42bc5dcfdc7
Thomas Rutzer <thomas.rutzer@artcom.de>
2020-11-24 23:38:33 +0100

    Add arrow icon to Tabs

--- src/components/tabs/index.js
+++ src/components/tabs/index.js
@@ -1,18 +1,30 @@
-import React from "react"
+import React, { useCallback, useRef } from "react"
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs'
 
 export default ({ children, defaultIndex }) => {
   return (
-    <Tabs defaultIndex={defaultIndex} className="tabs">
-      <TabList className="tabs__tab-list">
+    <Tabs
+      defaultIndex={defaultIndex}
+      className="tabs py">
+      <TabList className="tab-list text-sm mb-5">
         {children.map((tab, key) =>
           <Tab
             key={key}
-            className="tabs__tab"
-            selectedClassName="tabs__tab--selected">
+            className={`tab-list__item tab-list__item--${key}`}
+            selectedClassName="tab-list__item--selected">
             {tab.props.label}
+            <svg
+              className="tab-list__icon relative"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              height="16"
+              width="16">
+              <path d="M0 0h24v24H0V0z" fill="none" />
+              <path d="M20 12l-1.41-1.41L13 16.17V4h-2v12.17l-5.58-5.59L4 12l8 8 8-8z" />
+            </svg>
           </Tab>
         )}
+
       </TabList>
 
       {children.map((tab, key) =>
