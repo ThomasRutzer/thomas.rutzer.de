3d720545855210d0930241b67809c8d310371ac8
Thomas Rutzer <thomas.rutzer@artcom.de>
2020-11-26 19:12:25 +0100

    Add back link to Intro of archive

--- src/components/sections/projectArchiveIntro.js
+++ src/components/sections/projectArchiveIntro.js
@@ -2,30 +2,51 @@ import React from "react"
 
 import ParallaxCircles from "./../parallaxCircles"
 import { ContentWrapper } from "../layout"
-
+import { InternalLink } from "./../links"
 
 const ProjectArchiveTitle = () => {
   return (
     <div className="project-archive-intro relative">
-      <ContentWrapper>
-        <h1 className="project-archive-intro__title leading-tight ">
-          <span className="text-sm text-grey-lighter">(Almost all)</span>
-          <span className="text-6xl font-bold block">Design <br />& Code</span>
-          <span className="text-5xl text-stroke text-stroke-white block">along the road</span>
-        </h1>
-      </ContentWrapper>
+      <ContentWrapper verticalSpacing={ false }>
+        <ul className="py-2">
+          <li>
+            <InternalLink link="/">
+              <svg
+                className="external-link__icon transform -rotate-90 mr-1"
+                xmlns="http://www.w3.org/2000/svg"
+                width="1em"
+                height="1em"
+                viewBox="0 0 24 24"
+                strokeWidth="2"
+                strokeLinecap="square"
+                strokeLinejoin="arcs"
+              >
+                <path d="M7 17l9.2-9.2M17 17V7H7" />
+              </svg>
+            Go to start view
+          </InternalLink>
+          </li>
+        </ul>
+        </ContentWrapper>
+        <ContentWrapper>
+          <h1 className="project-archive-intro__title leading-tight md:my-4">
+            <span className="text-sm text-grey-lighter">(Almost all)</span>
+            <span className="text-6xl font-bold block">Design <br />& Code</span>
+            <span className="text-5xl text-stroke text-stroke-white block">along the road</span>
+          </h1>
+        </ContentWrapper>
 
-      <div className="project-archive-intro__bg bg-gradient-to-r from-black ">
-        <div className="project-archive-intro__circle project-archive-intro__circle--1">
-          <ParallaxCircles mouseMoveTranslations={[1, 3, 1, 3]} />
+        <div className="project-archive-intro__bg bg-gradient-to-r from-black" aria-hidden="true">
+          <div className="project-archive-intro__circle project-archive-intro__circle--1">
+            <ParallaxCircles mouseMoveTranslations={[1, 3, 1, 3]} />
+          </div>
+          <div className="project-archive-intro__circle project-archive-intro__circle--2">
+            <ParallaxCircles mouseMoveTranslations={[5, 10, 5, 10]} />
+          </div>
+          <div className="project-archive-intro__circle project-archive-intro__circle--3">
+            <ParallaxCircles />
+          </div>
         </div>
-        <div className="project-archive-intro__circle project-archive-intro__circle--2">
-          <ParallaxCircles mouseMoveTranslations={[5, 10, 5, 10]} />
-        </div>
-        <div className="project-archive-intro__circle project-archive-intro__circle--3">
-          <ParallaxCircles />
-        </div>
-      </div>
     </div>
   )
 }
