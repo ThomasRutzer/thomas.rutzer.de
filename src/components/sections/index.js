9f13e21c8840230328ad801b7cdac1ac98fc0d93
Thomas Rutzer <thomas.rutzer@artcom.de>
2020-11-26 22:31:30 +0100

    Remove SectionDigArchive

--- src/components/sections/index.js
+++ src/components/sections/index.js
@@ -1,9 +1,7 @@
-import SectionDigArchive from "./sectionDigArchive"
 import SectionTitle from "./sectionTitle"
 import WorkSectionWithImageGrid from "./workSectionWithImageGrid"
 
 export {
-  SectionDigArchive,
   SectionTitle,
   WorkSectionWithImageGrid
 }
