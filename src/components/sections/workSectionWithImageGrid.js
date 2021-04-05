84fb835536f7a769c3bfb1afd74f63bd5f94f13f
Thomas Rutzer <thomas.rutzer@artcom.de>
2021-04-05 22:52:16 +0200

    Remove property "selectedAppearance" from works.json

--- src/components/sections/workSectionWithImageGrid.js
+++ src/components/sections/workSectionWithImageGrid.js
@@ -1,34 +1,27 @@
 import React from "react"
-import classNames from "classnames"
 
 import { ContentWrapper } from "./../layout"
 import { ExternalLink } from "./../links"
 import ImageGrid from "./../imageGrid"
 import FeatureList from "./../featureList"
 
-const WorkSectionWithImageGrid = ({ work }) => {
-  const bgClass = classNames({
-    "bg-grey-darkest": work.selectedAppearance === "dark",
-    "bg-secondary": work.selectedAppearance === "secondary"
-  })
-  return (
-    <section className={bgClass}>
+const WorkSectionWithImageGrid = ({ work, appearance }) => 
+    <section className={appearance === "primary" ? "bg-grey-darkest" : "bg-secondary"}>
       <ContentWrapper
         variant="large"
         additionalClasses="px-5 pt-6 md:pb-6 md:pt-7"
         verticalSpacing={false}
         horizontalSpacing={false}>
         <ImageGrid
-          appearance={work.selectedAppearance === "dark" ? "light" : "dark"}
+          category="project"
+          title={ work.title}
+          subTitle={work.subTitle}
           images={work.images.map((image, key) => ({
             gatsbyImageData: work.fields.workImageAssets[key].childImageSharp.gatsbyImageData,
             alt: image.alt,
             tiles: image.grid.tiles,
-            mobileRow: image.grid.mobileRow,
             fit: image.grid.fit
-          }))}
-          title={work.title}
-          subtitle={work.subTitle} />
+          }))} />
       </ContentWrapper>
       <ContentWrapper>
         <div className="grid grid-cols-6 md:gap-5 leading-7">
@@ -36,14 +29,14 @@ const WorkSectionWithImageGrid = ({ work }) => {
             className="flex md:flex-col justify-between text-sm col-start-1 col-span-6 md:col-span-1 mb-4 md:mb-0">
             <p>
               <span
-                className={`${work.selectedAppearance === "dark" ? "text-secondary-lighter" : "text-grey-darkest"} block`}>
+                className={`${appearance === "primary" ? "text-secondary-lighter" : "text-grey-darkest"} block`}>
                 first release
               </span>
               <span className="text-grey-lighter">{work.year}</span>
             </p>
             <div>
               <span
-                className={`${work.selectedAppearance === "dark" ? "text-secondary-lighter" : "text-grey-darkest"} block`}>
+                className={`${appearance === "primary" ? "text-secondary-lighter" : "text-grey-darkest"} block`}>
                 key facts
               </span>
               <FeatureList items={work.features} />
@@ -74,7 +67,5 @@ const WorkSectionWithImageGrid = ({ work }) => {
         </div>
       </ContentWrapper>
     </section>
-  )
-}
 
 export default WorkSectionWithImageGrid
