1bf9c5753264fe68f38506cfc11aa7a36b815db3
Thomas Rutzer <thomas.rutzer@artcom.de>
2021-04-06 22:41:35 +0200

    Remove wrong closure in ContentWrapper > createClasses fn

--- src/components/layout/contentWrapper.js
+++ src/components/layout/contentWrapper.js
@@ -1,37 +1,36 @@
 import React from "react"
 import classnames from "classnames"
 
-function ContentWrapper(
-  { 
-    children, 
-    additionalClasses = "", 
-    variant, 
-    verticalSpacing = true, 
-    horizontalSpacing = true 
+const ContentWrapper = (
+  {
+    children,
+    additionalClasses = "",
+    variant,
+    verticalSpacing = true,
+    horizontalSpacing = true
   }
-) {
+) => {
   const createClasses = () => {
-    const maxWClass = (variant => {
+    const maxWClass = () => {
       switch (variant) {
-        case 'small':
+        case "small":
           return "max-w-4xl";
         case "large":
           return "max-w-full";
         default:
           return "max-w-6xl";
       }
-    })(variant);
+    }
 
     return classnames(
-      "relative", "container", "mx-auto",
       { "px-5": horizontalSpacing },
       { "py-6 md:py-7": verticalSpacing },
-      { [`${maxWClass}`]: true },
+      { [`${maxWClass()}`]: true },
     )
   }
 
   return (
-    <div className={`${createClasses()} ${additionalClasses}`}>
+    <div className={`relative container mx-auto ${createClasses()} ${additionalClasses}`}>
       {children}
     </div>
   )
