cb5c8eba335c39ff831227b04d50a95d97508a44
Thomas Rutzer <thomas@rutzer.de>
2021-04-07 21:22:54 +0200

    Feature/add collections data (#23)
    * Add collections resolver and data
    
    * Clean up gatsby-config
    
    * Update dep gatsby-plugin-sharp
    
    * Add image lazy loading
    
    * Add tag to headline of ImageGrid
    
    * Update collections data
    
    * Remove property "selectedAppearance" from works.json
    
    * Query more collections data in home page
    
    * Add section CollectionSectionWithImageGrid
    
    * Add prop category to ImageGrid
    
    * ImageGrid content spacing
    
    * More collections data
    
    * Spacing of image text blocks
    
    * Template of CollectionSectionWithImageGrid
    
    * Update image data of work item "schere, stein, papier"
    
    * Fix tiles of images in CollectionSectionWithImageGrid
    
    * Update works data
    
    * Collection content updates
    
    * Increase spacing of ImageGrid textblocks on small screens
    
    * Fix spacings
    
    Co-authored-by: Thomas Rutzer <thomas.rutzer@artcom.de>

--- src/components/imageGrid/index.js
+++ src/components/imageGrid/index.js
@@ -4,7 +4,7 @@ import Image from "./image"
 import { useMousemoveTranslation } from "./../../hooks"
 import isTouchDevice from "./../../utils/isTouchDevice"
 
-const ImageGrid = ({ appearance, title, subtitle, images = [] }) => {
+const ImageGrid = ({ category, title, subTitle, images = [] }) => {
   const [disableImgTranslations, setDisableImgTranslations] = useState(true)
   const translations = useMousemoveTranslation(10, 20, 10, 20, disableImgTranslations)
 
@@ -18,15 +18,19 @@ const ImageGrid = ({ appearance, title, subtitle, images = [] }) => {
   }, [])
 
   return (
-    <div className="relative"  {...mouseEvents}>
-      <div className={`flex items-center justify-center flex-col lg:absolute lg:w-full lg:h-full z-10 mb-6 pointer-events-none`}>
-        <h3 className="text-4xl md:text-6xl md:text-stroke-white md:text-stroke-2 italic">{title}</h3>
-        <h4>{subtitle}</h4>
+    <div className="relative" {...mouseEvents}>
+      <div className={
+        `flex items-center justify-center flex-col text-center lg:absolute lg:w-full lg:h-full z-10 mb-6 pointer-events-none`
+      }>
+        <h5 className="bg-primary text-xs italic rounded-md px-1">{category}</h5>
+        <h3 className="text-4xl md:text-6xl md:text-stroke-white md:text-stroke-2 italic leading-snug">
+          {title}
+        </h3>
+        <h4>{subTitle}</h4>
       </div>
       <div className="grid grid-cols-6 lg:grid-rows-3 lg:gap-5 lg:h-screen">
         {images.map(({ gatsbyImageData, alt, fit, tiles }, key) =>
           <Image
-            appearance={appearance}
             key={key}
             index={key}
             gatsbyImageData={gatsbyImageData}
--- src/components/sections/collectionSectionWithImageGrid.js
+++ src/components/sections/collectionSectionWithImageGrid.js
@@ -1,11 +1,12 @@
 import React from "react"
 
-import { ContentWrapper } from "./../layout"
-import { ExternalLink } from "./../links"
-import ImageGrid from "./../imageGrid"
+import { ContentWrapper } from "../layout"
+import { ExternalLink } from "../links"
+import ImageGrid from "../imageGrid"
 import FeatureList from "./../featureList"
 
-const WorkSectionWithImageGrid = ({ work, appearance }) => 
+const CollectionSectionWithImageGrid = ({ collection, appearance }) => {
+  return (
     <section className={appearance === "primary" ? "bg-grey-darkest" : "bg-secondary"}>
       <ContentWrapper
         variant="large"
@@ -13,46 +14,40 @@ const WorkSectionWithImageGrid = ({ work, appearance }) =>
         verticalSpacing={false}
         horizontalSpacing={false}>
         <ImageGrid
-          category="project"
-          title={ work.title}
-          subTitle={work.subTitle}
-          images={work.images.map((image, key) => ({
-            gatsbyImageData: work.fields.workImageAssets[key].childImageSharp.gatsbyImageData,
-            alt: image.alt,
-            tiles: image.grid.tiles,
-            fit: image.grid.fit
+          category="collection"
+          title={collection.title}
+          subTitle={collection.subTitle}
+          images={collection.includedWorks.map((work, key) => ({
+            gatsbyImageData: work.fields.workImageAssets[0].childImageSharp.gatsbyImageData,
+            alt: `${work.title} – ${work.subTitle} (${work.year})`,
+            tiles: collection.imageGrid.tiles[key],
+            fit: work.fields.workImageAssets[0].fit
           }))} />
       </ContentWrapper>
       <ContentWrapper>
         <div className="grid grid-cols-6 md:gap-5 leading-7">
           <div
             className="flex md:flex-col justify-between text-sm col-start-1 col-span-6 md:col-span-1 mb-4 md:mb-0">
-            <p>
-              <span
-                className={`${appearance === "primary" ? "text-secondary-lighter" : "text-grey-darkest"} block`}>
-                first release
-              </span>
-              <span className="text-grey-lighter">{work.year}</span>
-            </p>
             <div>
               <span
                 className={`${appearance === "primary" ? "text-secondary-lighter" : "text-grey-darkest"} block`}>
                 key facts
               </span>
-              <FeatureList items={work.features} />
+              <FeatureList items={collection.features} />
             </div>
           </div>
           <div className="my-4 md:my-0 col-start-1 col-span-6 md:col-start-2 md:col-span-4">
-            <p className="whitespace-pre-line">{work.leadingDescription}</p>
+            <p className="whitespace-pre-line">{collection.leadingDescription}</p>
             <p className="whitespace-pre-line mt-3">
-              {work.description}
+              {collection.description}
             </p>
           </div>
           <div className="col-start-1 col-span-6 md:col-start-6 md:col-span-1">
             <div className="flex flex-col md:items-end">
               <div
-                className="md:transform md:-rotate-90 md:-translate-y-full md:origin-bottom-right">
-                {work.links.map(({ link, label }, key) =>
+                className="md:transform md:-rotate-90 md:-translate-y-full md:origin-bottom-right"
+              >
+                {collection.links.map(({ link, label }, key) =>
                   <div className="whitespace-no-wrap" key={key}>
                     <ExternalLink
                       appearance="primary"
@@ -67,5 +62,7 @@ const WorkSectionWithImageGrid = ({ work, appearance }) =>
         </div>
       </ContentWrapper>
     </section>
+  )
+}
 
-export default WorkSectionWithImageGrid
+export default CollectionSectionWithImageGrid
