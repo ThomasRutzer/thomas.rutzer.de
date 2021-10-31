db8746c3f65d4e15a09c3503732e92e056114573
Thomas Rutzer <hi@thomasrutzer.dev>
2021-10-31 15:33:46 +0100

    Update css spacing of Title in Intro

--- src/components/intro/title.js
+++ src/components/intro/title.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 export default React.forwardRef((props, ref) => (
-  <h1 ref={ref} className="intro__title text-primary">
+  <h1 ref={ref} className="intro__title text-primary md:-mt-4">
     {props.children}
   </h1>
 ))
