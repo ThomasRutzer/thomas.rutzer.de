0397cf199543612237fe2d7924c7f0b877567b5b
Thomas Rutzer <hi@thomasrutzer.dev>
2022-02-25 19:12:52 +0100

    Add font-mono to SectionTitle

--- src/components/sectionTitle/index.js
+++ src/components/sectionTitle/index.js
@@ -4,7 +4,7 @@ import "./index.scss"
 
 const SectionTitle = ({ children }) => {
   return (
-    <h2 className="section-title relative inline-flex items-center gap-2 md:gap-2 text-sm">
+    <h2 className="section-title font-mono relative inline-flex items-center gap-2 md:gap-2 text-sm">
       <span>{children}</span>
     </h2>
   )
