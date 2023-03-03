39d5d9f94237a978b2d1ae32047805f6578f2c57
Thomas Rutzer <hi@thomasrutzer.dev>
2023-03-03 21:42:03 +0100

    Add prop fullWidth to  SectionTitle

--- src/components/sectionTitle/sectionTitle.js
+++ src/components/sectionTitle/sectionTitle.js
@@ -1,8 +1,13 @@
+import classnames from "classnames"
 import React from "react"
 
-const Root = ({ children }) => {
+const Root = ({ children, fullWidth = false }) => {
+  const classNames = classnames(
+    "section-title font-mono relative inline-flex items-center gap-2 md:gap-2 text-sm uppercase",
+    { "w-full": fullWidth }
+  )
   return (
-    <h2 className="section-title font-mono relative inline-flex items-center gap-2 md:gap-2 text-sm">
+    <h2 className={classNames}>
       <span>{children}</span>
     </h2>
   )
